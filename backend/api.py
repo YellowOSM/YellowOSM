b0ed741ceda553f5f3a8cc280b3eada44dfb71bb
Florian Klien <flowolf@klienux.org>
2019-01-11 11:34:45 +0100

    handling async background tasks, returning error codes (#5)

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,6 @@
+import time
+import json
+
 import responder
 
 api = responder.API()
@@ -18,6 +21,36 @@ def hello_user(req, resp, *, user):
 def hello_json(req, resp, *, user):
     user = user.strip("/")
     resp.media = {"hello": user}
+
+@api.route("/error")
+def error(req, resp):
+    resp.headers['X-Answer'] = '42'
+    resp.status_code = 415
+    resp.text = "ooops"
+
+@api.route("/expensive-task")
+async def handle_task(req, resp):
+
+    @api.background.task
+    def process_data(data):
+        """This can take some time"""
+        print("starting background task...")
+        time.sleep(5)
+        print("finished background task...")
+
+
+    # parse incoming data form-encoded
+    # json and yaml automatically work
+    try:
+        data = await req.media()
+    except json.decoder.JSONDecodeError:
+        data = None
+        pass
+
+    process_data(data)
+
+    resp.media = {'success': True}
+
 
 if __name__ == '__main__':
     api.run()
