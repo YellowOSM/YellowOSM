93783a7bab1657d65a6b1a56d0b3bef4bd2e88d1
Daniel Lamprecht <dl@love-it.at>
2019-01-25 15:06:43 +0100

    #14: add basic about page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {YellowmapComponent} from "./yellowmap/yellowmap.component";
+import {YellowmapComponent} from './yellowmap/yellowmap.component';
+import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
-  {path: '', component: YellowmapComponent}
+  {path: '', component: YellowmapComponent},
+  {path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {YellowmapComponent} from './yellowmap/yellowmap.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    YellowmapComponent
+    YellowmapComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
