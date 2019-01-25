4019fb59a4b854f600390313511fcfd40be8e7c1
Daniel Lamprecht <dl@love-it.at>
2019-01-25 14:23:13 +0100

    ansible#4 add script for elasticsearch

---
+++ backend/scripts/elasticsearch.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# see https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-point.html
+
+# generate test file at https://www.json-generator.com/ with
+# [{'repeat(50)': {name: '{{firstName()}} {{surname()}}',address: '{{integer(100, 999)}} {{street()}},{{city()}}, {{state()}}, {{integer(100, 10000)}}',location: "{{floating(47.055593, 47.055593)}},{{floating(15.405622, 15.405622)}}",description: function (tags) {var fruits = ['pharmacy', 'restaurant','bar'];return fruits[tags.integer(0, fruits.length - 1)];}}}]
+# and save as graz.json
+
+# BASE_URL='localhost:9200'
+BASE_URL='https://es.yosm.at:9200'
+
+# delete yosm index
+curl -X DELETE "{$BASE_URL}/yosm?pretty"
+
+# create yosm index
+# curl -X PUT "localhost:9200/yosm?pretty"
+
+# create index and set field mapping
+# see https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html
+curl -X PUT "{$BASE_URL}/yosm?pretty" -H 'Content-Type: application/json' -d'
+{
+    "mappings": {
+        "_doc": {
+            "properties": {
+                "location": {
+                    "type": "geo_point"
+                }
+            }
+        }
+    }
+}
+'
+
+# load sample data set
+curl -H "Content-Type: application/json" -XPOST "{$BASE_URL}/yosm/_doc/_bulk?pretty&refresh" --data-binary "@graz.json"
+
+# get index status
+curl "{$BASE_URL}/_cat/indices?v"
+
+# search index
+curl "{$BASE_URL}/yosm/_search?q=*&pretty"
+
+# search index by geo bounding box
+curl "{$BASE_URL}/yosm/_search" -H 'Content-Type: application/json' -d'
+{
+  "query": {
+    "geo_bounding_box": {
+      "location": {
+        "top_left": {
+          "lat": 48,
+          "lon": 10
+        },
+        "bottom_right": {
+          "lat": 40,
+          "lon": 16
+        }
+      }
+    }
+  }
+}
+'
+
+# https://grokonez.com/frontend/angular/angular-6/angular-6-elasticsearch-example-quick-start-how-to-add-elasticsearch-js
+# CORS für Elasticsearch erlauben
