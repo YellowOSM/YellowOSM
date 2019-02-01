424e93ca29e131fb48bc07e6768efc3df6ec4907
Florian Klien <flowolf@klienux.org>
2019-02-01 12:54:35 +0100

    pbf_import

--- backend/scripts/osm2pgsql/pbf_import.sh
+++ backend/scripts/osm2pgsql/pbf_import.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-dropdb gis
-createdb gis
-psql -d gis -c 'CREATE EXTENSION postgis; CREATE EXTENSION hstore;'
-osm2pgsql --create --database gis Graz.osm.pbf --style yosm.style
+# pbffile="/home/flo/Downloads/Graz.osm.pbf"
+pbffile="/home/flo/Downloads/austria-latest.osm.pbf"
+# sudo -u postgres dropdb gis
+# sudo -u postgres createdb gis
+# sudo -u postgres psql -d gis -c 'CREATE EXTENSION postgis; CREATE EXTENSION hstore;'
+# sudo -u postgres psql -c "grant all privileges on database gis to flo;"
+time osm2pgsql -U flo -C 600 --create --database gis $pbffile --style yosm.style
