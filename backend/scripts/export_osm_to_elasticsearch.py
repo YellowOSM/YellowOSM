1f63400ed9c561188c470d6b70496e750a011ed1
Florian Klien <flowolf@klienux.org>
2019-02-01 12:55:04 +0100

    #11: osm export to elasticsearch adapted queries

--- backend/scripts/export_osm_to_elasticsearch.py
+++ backend/scripts/export_osm_to_elasticsearch.py
@@ -8,7 +8,9 @@ EXPORT_ES_FILE = "/home/flo/osm_es_export.json"
 
 # also export polygons
 poly = True
+poly = False
 query_db = True
+query_db = False
 LIMIT = 100000000000
 # LIMIT = 10
 
@@ -17,9 +19,9 @@ LIMIT = 100000000000
 # COMMAND = "sudo -u postgres psql -d gis -c \"\copy (SELECT p.name,l.name,lon,lat,p.{key},l.{key} FROM planet_osm_nodes AS n LEFT JOIN planet_osm_point AS p ON n.id = p.osm_id LEFT JOIN planet_osm_polygon AS l ON n.id = l.osm_id WHERE p.{key} = '{value}' OR l.{key} = '{value}' LIMIT {limit}) TO STDOUT With CSV;\" | cat >> {file}"
 
 COMMAND1 = """sudo -u postgres psql -d gis -c \"\copy (
-        SELECT p.name,lon,lat,p.{key}
-        FROM planet_osm_nodes AS n
-        LEFT JOIN planet_osm_point AS p ON n.id = p.osm_id
+        SELECT p.name,st_x(st_transform(p.way, 4326)),st_y(st_transform(p.way, 4326)),p.{key},
+        *
+        FROM planet_osm_point AS p
         WHERE p.{key} = '{value}'
         LIMIT {limit}
         ) TO STDOUT With CSV;\" | cat >> {file}"""
@@ -27,10 +29,9 @@ COMMAND1 = """sudo -u postgres psql -d gis -c \"\copy (
 COMMAND2, COMMAND3 = None, None
 if poly:
     COMMAND2 = """sudo -u postgres psql -d gis -c \"\copy (
-        SELECT p.name,n.lon,n.lat,p.{key}
+        SELECT p.name,st_x(st_transform(st_centroid(p.way), 4326)),st_y(st_transform(st_centroid(p.way), 4326)),p.{key},
+        *
         FROM planet_osm_polygon as p
-        LEFT JOIN planet_osm_ways as w ON p.osm_id = w.id
-        LEFT JOIN planet_osm_nodes as n ON n.id = w.nodes[1]
         WHERE p.{key} = '{value}'
         LIMIT {limit}
         ) TO STDOUT With CSV;\" | cat >> {file}"""
@@ -38,50 +39,68 @@ if poly:
 
     # multipolygons
     # mulitpolygons get a negative osm_id in the polygon table!!
-    COMMAND3 = """sudo -u postgres psql -d gis -c \"\copy (
-        SELECT p.name,n.lon,n.lat,p.{key}
-        FROM planet_osm_polygon as p
-        LEFT JOIN planet_osm_rels as r ON p.osm_id*-1 = r.id
-        LEFT JOIN planet_osm_ways as w ON r.parts[1] = w.id
-        LEFT JOIN planet_osm_nodes as n ON w.nodes[1] = n.id
-        WHERE ( p.{key} = '{value}' AND p.osm_id < 0 )
-        LIMIT {limit}
-        ) TO STDOUT With CSV;\" | cat >> {file}"""
-        # """
-
+    # COMMAND3 = """sudo -u postgres psql -d gis -c \"\copy (
+    #     SELECT p.name,st_x(st_transform(st_centroid(p.way), 4326)),st_y(st_transform(st_centroid(p.way), 4326)),p.{key}
+    #     FROM planet_osm_polygon as p
+    #     LEFT JOIN planet_osm_rels as r ON p.osm_id*-1 = r.id
+    #     LEFT JOIN planet_osm_ways as w ON r.parts[1] = w.id
+    #     LEFT JOIN planet_osm_nodes as n ON w.nodes[1] = n.id
+    #     WHERE ( p.{key} = '{value}' AND p.osm_id < 0 )
+    #     LIMIT {limit}
+    #     ) TO STDOUT With CSV;\" | cat >> {file}"""
+    #     # """
 
 COMMAND4 = """sudo -u postgres psql -d gis -c \"\copy (
-            SELECT p.name,lon,lat,p.{key}
-            FROM planet_osm_nodes AS n
-            LEFT JOIN planet_osm_point AS p ON n.id = p.osm_id
-            WHERE (p.{key} is not null AND p.{key} != 'vacant')
-            LIMIT {limit}
-            ) TO STDOUT With CSV;\" | cat >> {file}"""
-            # """
-COMMAND5, COMMAND6 = None, None
-if poly:
-    COMMAND5 = """sudo -u postgres psql -d gis -c \"\copy (
-        SELECT p.name,n.lon,n.lat,p.{key}
-        FROM planet_osm_polygon as p
-        LEFT JOIN planet_osm_ways as w ON p.osm_id = w.id
-        LEFT JOIN planet_osm_nodes as n ON n.id = w.nodes[1]
+        SELECT p.name,st_x(st_transform(p.way, 4326)),st_y(st_transform(p.way, 4326)),p.{key},
+        *
+        FROM planet_osm_point AS p
         WHERE (p.{key} is not null AND p.{key} != 'vacant')
         LIMIT {limit}
         ) TO STDOUT With CSV;\" | cat >> {file}"""
         # """
 
-    COMMAND6 = """sudo -u postgres psql -d gis -c \"\copy (
-        SELECT p.name,n.lon,n.lat,p.{key}
+
+# COMMAND4 = """sudo -u postgres psql -d gis -c \"\copy (
+#         SELECT p.name,st_x(st_transform(p.way, 4326)),st_y(st_transform(p.way, 4326)),p.{key}
+#             FROM planet_osm_nodes AS n
+#             LEFT JOIN planet_osm_point AS p ON n.id = p.osm_id
+#             WHERE (p.{key} is not null AND p.{key} != 'vacant')
+#             LIMIT {limit}
+#             ) TO STDOUT With CSV;\" | cat >> {file}"""
+#             # """
+COMMAND5, COMMAND6 = None, None
+if poly:
+    COMMAND5 = """sudo -u postgres psql -d gis -c \"\copy (
+        SELECT p.name,st_x(st_transform(st_centroid(p.way), 4326)),st_y(st_transform(st_centroid(p.way), 4326)),p.{key},\
+        *
         FROM planet_osm_polygon as p
-        LEFT JOIN planet_osm_rels as r ON p.osm_id*-1 = r.id
-        LEFT JOIN planet_osm_ways as w ON r.parts[1] = w.id
-        LEFT JOIN planet_osm_nodes as n ON w.nodes[1] = n.id
-        WHERE ( p.{key} is not null AND p.{key} != 'vacant' AND p.osm_id < 0 )
+        WHERE (p.{key} is not null AND p.{key} != 'vacant')
         LIMIT {limit}
         ) TO STDOUT With CSV;\" | cat >> {file}"""
         # """
+    # COMMAND5 = """sudo -u postgres psql -d gis -c \"\copy (
+    #     SELECT p.name,n.lon,n.lat,p.{key}
+    #     FROM planet_osm_polygon as p
+    #     LEFT JOIN planet_osm_ways as w ON p.osm_id = w.id
+    #     LEFT JOIN planet_osm_nodes as n ON n.id = w.nodes[1]
+    #     WHERE (p.{key} is not null AND p.{key} != 'vacant')
+    #     LIMIT {limit}
+    #     ) TO STDOUT With CSV;\" | cat >> {file}"""
+    #     # """
+    #
+    # COMMAND6 = """sudo -u postgres psql -d gis -c \"\copy (
+    #     SELECT p.name,n.lon,n.lat,p.{key}
+    #     FROM planet_osm_polygon as p
+    #     LEFT JOIN planet_osm_rels as r ON p.osm_id*-1 = r.id
+    #     LEFT JOIN planet_osm_ways as w ON r.parts[1] = w.id
+    #     LEFT JOIN planet_osm_nodes as n ON w.nodes[1] = n.id
+    #     WHERE ( p.{key} is not null AND p.{key} != 'vacant' AND p.osm_id < 0 )
+    #     LIMIT {limit}
+    #     ) TO STDOUT With CSV;\" | cat >> {file}"""
+    #     # """
 
-commands = [COMMAND1, COMMAND2, COMMAND3, COMMAND4, COMMAND5, COMMAND6]
+# commands = [COMMAND1, COMMAND2, COMMAND3, COMMAND4, COMMAND5, COMMAND6]
+commands = [COMMAND1, COMMAND2, COMMAND4, COMMAND5]
 
 export_amenity = { "key": "amenity",
                     "values" :
@@ -230,8 +249,39 @@ if query_db:
     #         os.system(command_now)
 
 
-# # format: name1,name2,lon,lat,type1,type2
-# format: name1,lon,lat,type1
+
+
+
+
+
+
+
+table = """
+ osm_id             | bigint               |           |          |
+ addr:city          | text                 |           |          |
+ addr:street        | text                 |           |          |
+ addr:housename     | text                 |           |          |
+ addr:housenumber   | text                 |           |          |
+ addr:interpolation | text                 |           |          |
+ opening_hours      | text                 |           |          |
+ website            | text                 |           |          |
+ contact:phone      | text                 |           |          |
+ contact:email      | text                 |           |          |
+ smoking            | text                 |           |          |
+ amenity            | text                 |           |          |
+ area               | text                 |           |          |
+ brand              | text                 |           |          |
+ building           | text                 |           |          |
+ service            | text                 |           |          |
+ name               | text                 |           |          |
+ shop               | text                 |           |          |
+ sport              | text                 |           |          |
+ tourism            | text                 |           |          |
+ leisure            | text                 |           |          |
+ way                | geometry(Point,3857) |           |          |
+"""
+
+# format: name1,lon,lat,type1,*
 with open(EXPORT_FILE,'r') as f, open(EXPORT_ES_FILE,'w') as out:
     reader = csv.reader(f, delimiter=',', quotechar='"')
     for line in reader:
@@ -240,6 +290,31 @@ with open(EXPORT_FILE,'r') as f, open(EXPORT_ES_FILE,'w') as out:
         # print(line)
         name = line[0]
         desc = line[3]
+        # Stern:
+        labels = [
+            "addr_city",
+            "addr_street",
+            "addr_housename",
+            "addr_housenumber",
+            "addr_interpolation",
+            "opening_hours",
+            "website",
+            "contact_phone",
+            "contact_email",
+            "smoking",
+            "amenity",
+            "area",
+            "brand",
+            "building",
+            "service",
+            "name",
+            "shop",
+            "sport",
+            "tourism",
+            "leisure",
+        ]
+        label_dict = {label: value for label,value in zip(labels,line[5:]) if value}
+
         if not name and desc:
             name = amend[desc][0] if desc in amend else desc.capitalize()
         if desc in amend:
@@ -249,8 +324,10 @@ with open(EXPORT_FILE,'r') as f, open(EXPORT_ES_FILE,'w') as out:
             continue
         # print(json.dumps({"index": {}}))
         out.write(json.dumps({"index": {}}) + "\n")
-        lat = int(line[2])/10000000
-        lon = int(line[1])/10000000
+        # lat = int(line[2])/10000000
+        # lon = int(line[1])/10000000
+        lat = float(line[2])
+        lon = float(line[1])
         # # print({"name": line[0], "location": [lon,lat], "description": line[3]})
         # print(json.dumps({"name": name, "location": [lon,lat], "description": desc}))
-        out.write(json.dumps({"name": name, "location": [lon,lat], "description": desc}) + "\n")
+        out.write(json.dumps({"name": name, "location": [lon,lat], "description": desc, "labels": label_dict}) + "\n")
