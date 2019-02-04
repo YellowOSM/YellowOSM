0c0e79770b30a10a4b9a6d9e3b1c0b939c9cb497
Florian Klien <flowolf@klienux.org>
2019-02-04 20:28:08 +0100

    fix path #39

--- backend/scripts/osm2pgsql/cron_import_pbf.sh
+++ backend/scripts/osm2pgsql/cron_import_pbf.sh
@@ -9,7 +9,7 @@ fi
 docker exec $(docker ps | grep yosm_postgres | awk '{print $1}') psql -U postgres -d gis -c 'CREATE EXTENSION postgis; CREATE EXTENSION hstore;'
 sleep 2
 # time osm2pgsql -U flo -C 1200 --create --database gis $pbffile --style yosm.style
-cd ~/osm2pgsql
+cd osm2pgsql
 # export POSTGRES_PASSWORD=`cat ~/.pgpass | cut -d : -f 5`
 export PGPASSWORD=`cat ~/.pgpass | cut -d : -f 5`
 time osm2pgsql -H 127.0.0.1 -U postgres -C 3000 --slim --create --database gis $pbffile --style yosm.style
