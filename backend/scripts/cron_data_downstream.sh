0b072e0084c4195a95f7b52d360795902430f5ef
Florian Klien <flowolf@klienux.org>
2019-02-09 20:23:35 +0100

    backend: cron_data_downstream cleanup

--- backend/scripts/cron_data_downstream.sh
+++ backend/scripts/cron_data_downstream.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -e
 logfile=/var/log/yosm_downstream.log
-pwd
-cd /root/docker-compose/responder/scripts
-pwd
+backend_scripts=/root/docker-compose/responder/scripts
+
+set -e
+pwd >> $logfile 2>&1
+cd $backend_scripts
+pwd >> $logfile 2>&1
 bash ./osm2pgsql/cron_import_pbf.sh >> $logfile 2>&1
 python3 ./export_osm_to_elasticsearch.py >> $logfile 2>&1
 bash ./elasticsearch_data_import.sh >> $logfile 2>&1
