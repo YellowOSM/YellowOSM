730f7de69afae548ae8637226b41de231a37dc38
Florian Klien <flowolf@klienux.org>
2019-02-28 16:57:20 +0100

    static: multiple fixes
    * fixed JS scrolling
    * new bg img
    * padding in menu bar

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,41 +1,41 @@
 (function($) {
 
   // Init Wow
-  wow = new WOW({
-    animateClass: 'animated',
-    offset: 100
-  });
-  wow.init();
+  // wow = new WOW({
+  //   animateClass: 'animated',
+  //   offset: 100
+  // });
+  // wow.init();
 
   $(".navbar-collapse a").on('click', function() {
     $(".navbar-collapse.collapse").removeClass('in');
   });
 
   // Navigation scrolls
-  $('.navbar-nav li a').bind('click', function(event) {
-    $('.navbar-nav li').removeClass('active');
-    $(this).closest('li').addClass('active');
-    var $anchor = $(this);
-    var nav = $($anchor.attr('href'));
-    if (nav.length) {
-      $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-
-      event.preventDefault();
-    }
-  });
-
-  // About section scroll
-  $(".overlay-detail a").on('click', function(event) {
-    event.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function() {
-      window.location.hash = hash;
-    });
-  });
+  // $('.navbar-nav li a').bind('click', function(event) {
+  //   $('.navbar-nav li').removeClass('active');
+  //   $(this).closest('li').addClass('active');
+  //   var $anchor = $(this);
+  //   var nav = $($anchor.attr('href'));
+  //   if (nav.length) {
+  //     $('html, body').stop().animate({
+  //       scrollTop: $($anchor.attr('href')).offset().top
+  //     }, 1500, 'easeInOutExpo');
+  //
+  //     event.preventDefault();
+  //   }
+  // });
+  //
+  // // About section scroll
+  // $(".overlay-detail a").on('click', function(event) {
+  //   event.preventDefault();
+  //   var hash = this.hash;
+  //   $('html, body').animate({
+  //     scrollTop: $(hash).offset().top
+  //   }, 900, function() {
+  //     window.location.hash = hash;
+  //   });
+  // });
 
   //jQuery to collapse the navbar on scroll
   $(window).scroll(function() {
