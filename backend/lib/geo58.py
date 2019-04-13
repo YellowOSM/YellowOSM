31853b39dcaf42c793aa93961b5bacacebaf0b65
Florian Klien <flowolf@klienux.org>
2019-04-13 18:01:48 +0200

    WIP Geo58 v3

--- backend/lib/geo58.py
+++ backend/lib/geo58.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import logging
+import sys
 
 log = logging.getLogger(__name__)
 log.setLevel('DEBUG')
+
+LOG_TO_CONSOLE = True
+if LOG_TO_CONSOLE:
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
 
 class Geo58():
     """Geo58 class to hold and convert geo-coordinates in z/x/y format and
@@ -19,15 +29,17 @@ class Geo58():
         self._zoom = zoom
         self._lat = lat or x
         self._lon = lon or y
+        self._int = None
         self._geo58 = g58
+
         log.debug("_init: {} {} {}".format(zoom, self._lat, self._lon))
-        log.debug("_init: {} {} {}".format(zoom, x, y))
+
         if not self._geo58:
             self._geo58 = self._coordsToGeo58(self._zoom, self._lat, self._lon)
-        log.debug("_init: {} {} {}".format(zoom, self._lat, self._lon))
 
         if (not self._zoom or not self._lat or not self._lon) and self._geo58:
             self._zoom, self._lat, self._lon = self._geo58ToCoords(self._geo58)
+            # log.debug("{} {} {}, geo58: {}".format(self._zoom, self._lat, self._lon, self._geo58))
             self._validate_coords(self._zoom, self._lat, self._lon)
 
         log.debug("_init: {} {} {}".format(self._zoom, self._lat, self._lon))
@@ -44,6 +56,7 @@ class Geo58():
 
     def _int2base58(self, number):
         """Convert an integer into a base58 encoded string"""
+        self._int = number
         base58str = []
         while number:
             base58str.append(Geo58.alph[int(number % 58)])
@@ -57,26 +70,45 @@ class Geo58():
         for c in b58_str:
             i *= 58
             i += Geo58.alph.index(c)
+
+        self._int = int(i)
+        log.debug(hex(self._int))
+
         return int(i)
 
-    def _validate_coords(self, zoom, x, y):
+    def _validate_coords(self, zoom, x, y):#, from_geo58=False):
+        """Coordinates must be within certain range:
+        zoom can be between [20 and 5],
+        x (lat) can be between  90.00000 and  -90.00000,
+        y (lon) can be between 180.00000 and -180.00000
+        """
         log.debug("validate: {} {} {}".format(zoom, x, y))
         x = int(float(x)*100000)
         y = int(float(y)*100000)
+        # log.debug("validate: {} {} {}".format(zoom, x, y))
+        # if from_geo58:
+        #     x = x - 9000000
+        #     y = y - 18000000
+        #     # log.debug("validate: {} {} {}".format(zoom, x, y))
         zoom = int(float(zoom) + 0.5) # round to closest full int
         if x < -9000000 or x > 9000000:
-            raise Geo58.Geo58Exception("lat (x) is out of range")
+            raise Geo58.Geo58Exception("lat (x) is out of range {} {} {}".format(zoom, x, y))
         if y < -18000000 or y > 18000000:
-            raise Geo58.Geo58Exception("lon (y) is out of range")
+            raise Geo58.Geo58Exception("lon (y) is out of range {} {} {}".format(zoom, x, y))
         if zoom < 5 or zoom > 20: # 4bit - 20 is no-zoom given
             raise Geo58.Geo58Exception("zoom is out of range (5-20)")
+        log.debug("valid: {} {} {}".format(zoom, x, y))
 
     def _convertCoordsToInt(self, x, y, z=20):
+        """Converts coordinates and zoom to one integer.
+        """
         log.debug("_convertCoordsToInt: {} {} {}".format(z, x, y))
         self._validate_coords(z,x,y)
 
-        x = int(float(x)*100000)
-        y = int(float(y)*100000)
+        # x is mapped from -90 to +90 -> 0 to 180
+        # y is mapped from -180 to 180 -> 0 to 360
+        x = int(float(x)*100000+  9000000)
+        y = int(float(y)*100000+ 18000000)
         z = int(float(z) + 0.5) # round to closest full int
         # # max values
         # x = -8999999
@@ -85,27 +117,17 @@ class Geo58():
 
         z = (z - 20) * -1 # zoom 20 = b0x0
 
-        minusx = True if x < 0 else False
-        minusy = True if y < 0 else False
-        x = abs(x)
-        y = abs(y)
-
-        coord = z << 51 | minusx << 50 | minusy << 49 | x << 25 | y
+        # TODO verschränken
+        coord = z << 51 | x << 26 | y #25 bits
         coord = int(coord)
         return coord
 
     def _convertIntToCoords(self, i):
-        zoom = (i & (15 << 2+24+25)) >> (51)
-        minusx = True if i & (1 << (50)) else False
-        minusy = True if i & (1 << (49)) else False
-        # minusx = True if i & (1 << (1+3+24+25)) else False
-        # minusy = True if i & (1 << (3+24+25)) else False
-        x = (i & 562949919866880) >> (25) # 2**24-1 << 25
-        y = i & 33554431 # 2**25-1
-        x = x*-1 if minusx else x
-        y = y*-1 if minusy else y
-        x = float(x)/100000
-        y = float(y)/100000
+        zoom = (i & (15 << 25+26)) >> (51)
+        x = (i & (2**25-1 << 26)) >> (26) # 2**24-1 << 25
+        y =  i & (2**26-1) # 67108863 # 2**25-1
+        x = float(x- 9000000)/100000
+        y = float(y-18000000)/100000
         z = (zoom *-1 + 20)
         z = int(z)
         log.debug("_convertIntToCoords: {} {} {}".format(z, x, y))
@@ -121,7 +143,7 @@ class Geo58():
         log.debug("_get58ToCoords: {} {} {}".format(z, x, y))
         return (z,x,y)
 
-#     x,y,zoom = coords
+# #     x,y,zoom = coords
 # def test(coords):
 #     print("\033[1m",coords,"\033[m")
 #     print("self._convertCoordsToInt: ", end="")
@@ -181,3 +203,61 @@ class Geo58():
 # test((x,y,zoom))
 # zoom,x,y = 19,90,-180
 # test((x,y,zoom))
+
+def test(lat=0.0,lon=0.0,zoom=5,coords=None):
+    if not coords:
+        log.debug("="*10+"> {}, {}, {}".format(lat,lon,zoom))
+        g58 = Geo58(lat=lat,lon=lon,zoom=zoom)
+    else:
+        log.debug("="*10+"> {}, {}, {}".format(coords[0],coords[1],coords[2]))
+        g58 = Geo58(lat=coords[0],lon=coords[1],zoom=coords[2])
+
+    # g58 = Geo58(lat=45.07071,lon=15.43951,zoom=5)
+    # print(hex(g58._int))
+    # print(g58._lat, g58._lon)
+    log.debug("="*10+"> "+Geo58(g58=g58._geo58)._geo58)
+
+
+def main():
+    test(lat=87.07071,lon=175.43951,zoom=15)
+    zoom,x,y = 19,10.12346,0.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 18,10.12347,0.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 17,47.12346,15.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 16,47.12346,15.12344
+    test(coords=(x,y,zoom))
+    zoom,x,y = 15,77.12346,15.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 14,-17.12346,150.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 13,7.12346,1.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 12,47.12346,15.12345
+    test(coords=(x,y,zoom))
+    # zoom,x,y = 11,47.12346,15.12345
+    # test(coords=(x,y,zoom))
+    zoom,x,y = 15,-47.12346,15.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 19,47.12346,-15.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 17,-47.12346,-15.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 14,-89.12346,-179.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 14,89.12346,-179.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 14,-89.12346,179.12345
+    test(coords=(x,y,zoom))
+    zoom,x,y = 12,90,180
+    test(coords=(x,y,zoom))
+    zoom,x,y = 19,-90,180
+    test(coords=(x,y,zoom))
+    zoom,x,y = 19,-90,-180
+    test(coords=(x,y,zoom))
+    zoom,x,y = 19,90,-180
+    test(coords=(x,y,zoom))
+
+if __name__ == "__main__":
+    main()
