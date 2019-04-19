74b3efd14be5f66564a29e4a8cf434f01c93055b
Florian Klien <flowolf@klienux.org>
2019-04-19 16:53:53 +0200

    added website crawler target script

---
+++ machine-learning/src/data/crawler/get_random_test_websites_with_meta_data.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import sys
+import json
+
+# grep website /tmp/osm_es_export.json | sed 's/^\(.*\)"osm_id": "\([^"]*\)".*website": "\([^"]*\)".*$/\2,\3/;' | shuf -n 200
+
+args = sys.argv
+
+if len(args) > 1:
+    datafile = args[1]
+else:
+    raise ValueError("""input file provide you must!\nrun:\n{} <your_input_file.json>""".format(args[0]))
+
+class Element():
+    def __init__(self, line):
+        self.data = json.loads(line)
+    def __repr__(self):
+        return str(self.data)
+
+with open(datafile,'r') as f:
+    for line in f.readlines():
+        if line.startswith('{"index": '):
+            continue
+        el = Element(line)
+        if 'website' in el.data['labels'] or 'contact_website' in el.data['labels']:
+            if 'phone' in el.data['labels']:
+                print(json.dumps(el.data))
+                # print(el)
+        # print(el)
+        # print(line,end="")
