2998fb57be4abc2bae4ff7483bd607dfe5858191
Florian Klien <flowolf@klienux.org>
2019-06-27 17:25:54 +0200

    add dates to log for cronjob

--- backend/scripts/cron_data_downstream.sh
+++ backend/scripts/cron_data_downstream.sh
@@ -3,12 +3,17 @@ logfile=/var/log/yosm_downstream.log
 backend_scripts=/root/docker-compose/responder/scripts
 
 set -e
+date >> $logfile 2>&1
 pwd >> $logfile 2>&1
 cd $backend_scripts
 pwd >> $logfile 2>&1
 cd osm2pgsql
 bash ./cron_import_pbf.sh          >> $logfile 2>&1
+date >> $logfile 2>&1
 cd ..
 python3 ./export_osm_to_elasticsearch.py     >> $logfile 2>&1
+date >> $logfile 2>&1
 bash ./elasticsearch_data_import.sh yosm     >> $logfile 2>&1
+date >> $logfile 2>&1
 bash ./elasticsearch_data_import.sh yosm_dev >> $logfile 2>&1
+date >> $logfile 2>&1
