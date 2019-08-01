42c5adf7b6c1dd07db2b90eae3e9a609e4f30415
Daniel Lamprecht <dl@love-it.at>
2019-08-01 17:19:05 +0200

    frontend: add manual service worker update check

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
-import { MatomoInjector } from 'ngx-matomo';
+import {Component, OnInit} from '@angular/core';
+import {MatomoInjector} from 'ngx-matomo';
 import {environment} from '../environments/environment';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
   constructor(
-    private matomoInjector: MatomoInjector
+    private matomoInjector: MatomoInjector,
+    private swUpdate: SwUpdate
   ) {
+  }
+
+  ngOnInit() {
     this.matomoInjector.init(environment.matomoBaseUrl, environment.matomoWebsiteId);
+    this.swUpdate.available.subscribe(event => {
+      console.log('A newer version is now available. Refresh the page now to update the cache');
+    });
+    console.log('checking for SW update...');
+    this.swUpdate.checkForUpdate();
   }
 }
