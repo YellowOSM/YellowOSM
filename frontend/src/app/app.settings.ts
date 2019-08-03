e72580f1965bc2ec1c37112f5c32efaecf391e30
Daniel Lamprecht <dl@love-it.at>
2019-08-03 17:43:20 +0200

    frontend: make mobile result list draggable all the way down

--- frontend/src/app/app.settings.ts
+++ frontend/src/app/app.settings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
   public static BREAKPOINT_DESKTOP = 768;
-  public static MIN_BOTTOM_OFFSET = 100;
+  public static MIN_BOTTOM_OFFSET = 25;
   public static MIN_TOP_OFFSET = 64;
   public static MIN_LEFT_OFFSET = 340;
 }
