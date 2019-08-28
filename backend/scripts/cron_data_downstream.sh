c95959f749d477ea07cff5e864a39bcb34bee0dd
Florian Klien <flowolf@klienux.org>
2019-08-28 10:47:05 +0200

    backend: fix PATH for pipenv, minor changes

--- backend/scripts/cron_data_downstream.sh
+++ backend/scripts/cron_data_downstream.sh
@@ -3,6 +3,10 @@ logfile=/var/log/yosm_downstream.log
 # backend_scripts=/root/docker-compose/responder/scripts
 backend_scripts=/root/backend/scripts/
 
+echo PATH >> $logfile
+echo $PATH >> $logfile
+PATH=$PATH:/usr/local/bin
+
 set -e
 date >> $logfile 2>&1
 pwd >> $logfile 2>&1
@@ -11,13 +15,24 @@ pwd >> $logfile 2>&1
 cd osm2pgsql
 bash ./cron_import_pbf.sh          >> $logfile 2>&1
 date >> $logfile 2>&1
+echo 'done with pbf import' >> $logfile 2>&1
+cd $backend_scripts
 cd ..
-pipenv install
+pwd
+echo 'running pipenv install' >> $logfile 2>&1
+# pipenv --bare install >> $logfile 2>&1
+pipenv install >> $logfile 2>&1
+
+cd scripts
+date >> $logfile 2>&1
+echo 'running export osm to elasticsearch' >> $logfile 2>&1
 pipenv run python3 ./export_osm_to_elasticsearch.py --split  >> $logfile 2>&1
 date >> $logfile 2>&1
-# fail with dev index first :)
+## fail with dev index first :)
+echo 'updating yosm_dev index' >> $logfile 2>&1
 bash ./elasticsearch_data_import.sh yosm_dev >> $logfile 2>&1
 date >> $logfile 2>&1
+echo 'updating yosm index' >> $logfile 2>&1
 bash ./elasticsearch_data_import.sh yosm     >> $logfile 2>&1
 date >> $logfile 2>&1
 echo "\nDone updating index!!\n\n" >> $logfile 2>&1
