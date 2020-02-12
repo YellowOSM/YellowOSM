e962c559179a135590365675616227b563ff7aa1
Florian Klien <flowolf@klienux.org>
2020-02-12 19:36:40 +0100

    removed wikimedia tiles, added hike/bike map as second tile option

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -11,9 +11,9 @@ export const environment = {
   // tileServerURL: '//tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png',
 
   tileServerURLs: [
-    {'label': 'Wikimedia', 'url': '//maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', 'attribution': '<a href="https://foundation.wikimedia.org/wiki/Maps_Terms_of_Use" target="_blank">CC-BY 4.0 Wikimedia</a> - <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'},
+    // {'label': 'Wikimedia', 'url': '//maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', 'attribution': '<a href="https://foundation.wikimedia.org/wiki/Maps_Terms_of_Use" target="_blank">CC-BY 4.0 Wikimedia</a> - <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'},
     {'label': 'HOT - Humanitarian map style', 'url': '//a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', 'attribution': '<a href="https://github.com/hotosm/HDM-CartoCSS" target="_blank">HOT CC0</a> - <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'},
-    // {'label': 'HikeBikeMap', 'url': '//tiles.wmflabs.org/hikebike/${z}/${x}/${y}.png', 'attribution': 'Map tiles by Carto, under CC BY 3.0. Data by <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, under ODbL.'},
+    {'label': 'HikeBikeMap', 'url': '//tiles.wmflabs.org/hikebike/${z}/${x}/${y}.png', 'attribution': 'Map tiles by Carto, under CC BY 3.0. Data by <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>, under ODbL.'},
     // {'label': 'ÖPNV-Karte', 'url': '//tile.memomaps.de/tilegen/${z}/${x}/${y}.png', 'attribution': 'Map <a href="https://memomaps.de/" target="_blank">memomaps.de</a> CC-BY-SA, map data <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> ODbL'},
   ],
 
